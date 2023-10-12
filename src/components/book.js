32f5c5dd1d8d78634250a21b890cb541036fc885
Erica Odeje <72512702+EricaOdeje@users.noreply.github.com>
2023-10-12 14:50:14 +0100

    Update book.js

--- src/components/book.js
+++ src/components/book.js
@@ -1,7 +1,10 @@
 import BookItem from "./bookItem";
 
+    {/*I used props as an argument to get data from read.js.*/}
 function Book(props){
+    {/*I used props to pass data from one component to another.*/}
     return props.myBook.map( 
+            {/*The map function is used to create a new aray.*/}
         (book) => {
             return <BookItem mybook={book} key={book.isbn}></BookItem>
         }
